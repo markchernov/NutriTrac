d7c1af5cb4d8edaa6e2dccad24c22d7c6a94b6fe
jleupp <jeffrey.leupp@gmail.com>
2016-03-17 16:36:35 -0600

    incremental controller method added to receive JSON from USDA

--- NutriTrac/src/controllers/NutriController.java
+++ NutriTrac/src/controllers/NutriController.java
@@ -1,11 +1,14 @@
 package controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import data.NutriTracRESTDAO;
+import entities.Food;
 
 @RestController
 //@SessionAttributes("loginCred")
@@ -28,6 +31,17 @@ public class NutriController {
 		@RequestMapping(path="ping")
 		public String ping() {
 			return "PONG";
+		}
+		
+		@ResponseBody
+		@RequestMapping(path="foodObj", method = RequestMethod.POST)
+		public boolean addNewFoodObj(@RequestBody Food foodJson) {
+			System.out.println("Food Number : " + foodJson.getNdbno() +
+							"\n Food Name : " + foodJson.getName() +
+							"\n Food Class : " + foodJson.getNutrients());
+			
+			//TODO fix return to return true if object was successfully added to database...
+			return true;
 		}
 
 }
