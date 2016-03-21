86baadac811a76e2706e856d4ebe57ad2dd13f52
jleupp <jeffrey.leupp@gmail.com>
2016-03-21 13:56:37 -0600

    Added meal, meal-detail, user and user_meal tables to database, the updated DB works with USDA scraper

--- nutritracdb.sql
+++ nutritracdb.sql
@@ -92,6 +92,114 @@ CREATE INDEX `FK_measures_nutrientId_idx` ON `measure` (`nutrient_id` ASC);
 
 SHOW WARNINGS;
 
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `user` (
+  `email` VARCHAR(100) NOT NULL,
+  `firstname` VARCHAR(45) NOT NULL,
+  `lastname` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `birthdate` DATE NULL,
+  `sex` VARCHAR(1) NULL,
+  PRIMARY KEY (`email`))
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `email_UNIQUE` ON `user` (`email` ASC);
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `meal`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `meal` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `meal` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `meal_details`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `meal_details` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `meal_details` (
+  `idmeal_details` INT NOT NULL AUTO_INCREMENT,
+  `meal_id` INT NOT NULL,
+  `food_id` INT NOT NULL,
+  `measure_id` INT NOT NULL,
+  PRIMARY KEY (`idmeal_details`),
+  CONSTRAINT `FK_mealdeet_mealid`
+    FOREIGN KEY (`meal_id`)
+    REFERENCES `meal` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_mealdeet_ndbno`
+    FOREIGN KEY (`food_id`)
+    REFERENCES `food` (`ndbno`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_mealdeet_measureid`
+    FOREIGN KEY (`measure_id`)
+    REFERENCES `measure` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+CREATE INDEX `FK_mealdeet_mealid_idx` ON `meal_details` (`meal_id` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `FK_mealdeet_ndbno_idx` ON `meal_details` (`food_id` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `FK_mealdeet_measureid_idx` ON `meal_details` (`measure_id` ASC);
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `user_meal`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_meal` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `user_meal` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `meal_id` INT NULL,
+  `user_email` VARCHAR(100) NULL,
+  `meal_date` DATETIME NULL,
+  `meal_category` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_UM_mealid`
+    FOREIGN KEY (`meal_id`)
+    REFERENCES `meal` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `FK_UM_useremail`
+    FOREIGN KEY (`user_email`)
+    REFERENCES `user` (`email`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+CREATE INDEX `FK_UM_mealid_idx` ON `user_meal` (`meal_id` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `FK_UM_useremail_idx` ON `user_meal` (`user_email` ASC);
+
+SHOW WARNINGS;
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
